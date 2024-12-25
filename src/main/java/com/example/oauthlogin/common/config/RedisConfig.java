cfab7e2d21e39d0d6e300ab9e1879d7fb72663bb
taehoy <rethnoly@gmail.com>
2024-12-26 00:25:32 +0900

    feat: redis 분기처리 (DEV, PROD)

--- src/main/java/com/example/oauthlogin/common/config/RedisConfig.java
+++ src/main/java/com/example/oauthlogin/common/config/RedisConfig.java
@@ -5,15 +5,31 @@ import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
 @EnableRedisRepositories
 public class RedisConfig {
+    private final Environment environment;
+
+    public RedisConfig(Environment environment) {
+        this.environment = environment;
+    }
+
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+
+        String activeProfile = environment.getProperty("spring.profiles.active");
+        if ("DEV".equals(activeProfile)) {
+            config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+        } else if ("PROD".equals(activeProfile)) {
+            config.useSingleServer().setAddress("redis://redis.example.com:6379");
+        } else {
+            throw new IllegalArgumentException("Unknown profile: " + activeProfile);
+        }
+
         return Redisson.create(config);
     }
 }
