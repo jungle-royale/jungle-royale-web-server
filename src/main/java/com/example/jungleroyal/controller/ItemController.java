d6b07e2d19cedf72fa536421cb8deec94f88786d
taehoy <rethnoly@gmail.com>
2024-12-31 00:25:56 +0900

    feat: Item create 로직 오류 수정 - @ResponseBody 제거

--- src/main/java/com/example/jungleroyal/controller/ItemController.java
+++ src/main/java/com/example/jungleroyal/controller/ItemController.java
@@ -6,7 +6,6 @@ import com.example.jungleroyal.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +16,7 @@ public class ItemController {
     private final ItemService itemService;
 
     @PostMapping("/create")
-    public ResponseEntity<ItemCreateResponse> createItem(@RequestBody ItemCreateRequest request) {
+    public ResponseEntity<ItemCreateResponse> createItem(ItemCreateRequest request) {
         ItemCreateResponse createdItem = itemService.createItem(request);
         return ResponseEntity.ok(createdItem);
     }
