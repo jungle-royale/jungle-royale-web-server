085af80d717ecf645d05b12e282afd2a5f46d89e
jjm159 <wjdwoaud15@gmail.com>
2024-12-31 17:46:58 +0900

    add: hellocontroller

---
+++ src/main/java/com/example/jungleroyal/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.jungleroyal.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    String hello() {
+        return "hello";
+    }
+}
