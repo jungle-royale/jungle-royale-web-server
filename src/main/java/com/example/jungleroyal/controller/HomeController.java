fc8d0295264c30754abe9e49169a45ae435992ce
taehoy <rethnoly@gmail.com>
2025-01-01 20:22:26 +0900

    feat: 로그인 테스트를 위한 홈화면 타임리프 적용

--- src/main/java/com/example/jungleroyal/controller/HomeController.java
+++ src/main/java/com/example/jungleroyal/controller/HomeController.java
@@ -1,12 +1,24 @@
 package com.example.jungleroyal.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    @Value("${kakao.client-id}")
+    private String kakaoClientId;
+
+    @Value("${kakao.redirect-uri}")
+    private String kakaoRedirectUri;
     @GetMapping
-    String home(){
+    String home(Model model){
+        // 모델에 데이터 추가
+        model.addAttribute("kakaoClientId", kakaoClientId);
+        model.addAttribute("kakaoRedirectUri", kakaoRedirectUri);
+
         return "home";
     }
 }
