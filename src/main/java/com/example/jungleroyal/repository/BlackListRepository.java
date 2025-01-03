61ff9091a8f87b57ccccfd8700856a2bece67acd
taehoy <rethnoly@gmail.com>
2025-01-03 17:26:53 +0900

    feat: BlackListJpaRepository 추상화 및 구현체 분리

--- src/main/java/com/example/jungleroyal/repository/BlackListRepository.java
+++ src/main/java/com/example/jungleroyal/repository/BlackListRepository.java
@@ -1,13 +1,7 @@
 package com.example.jungleroyal.repository;
 
-import com.example.jungleroyal.domain.BlackList;
-import org.springframework.data.jpa.repository.JpaRepository;
+public interface BlackListRepository {
+    boolean existsByInvalidRefreshToken(String refreshToken);
 
-import java.util.Optional;
-
-public interface BlackListRepository extends JpaRepository<BlackList, Long> {
-    default boolean existsByInvalidRefreshToken(String refreshToken) {
-        return findByInvalidRefreshToken(refreshToken).isPresent();
-    }
-    Optional<BlackList> findByInvalidRefreshToken(String refreshToken);
+    void save(BlackListJpaEntity blackListJpaEntity);
 }
---
+++ src/main/java/com/example/jungleroyal/repository/BlackListRepositoryImpl.java
@@ -0,0 +1,27 @@
+package com.example.jungleroyal.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class BlackListRepositoryImpl implements BlackListRepository {
+
+    private final BlackListJpaRepository blackListJpaRepository;
+
+    @Override
+    public boolean existsByInvalidRefreshToken(String refreshToken) {
+        // 입력값 검증
+        if (refreshToken == null || refreshToken.trim().isEmpty()) {
+            throw new IllegalArgumentException("Refresh token must not be null or empty.");
+        }
+
+        // JpaRepository 호출
+        return blackListJpaRepository.existsByInvalidRefreshToken(refreshToken);
+    }
+
+    @Override
+    public void save(BlackListJpaEntity blackListJpaEntity) {
+
+    }
+}
