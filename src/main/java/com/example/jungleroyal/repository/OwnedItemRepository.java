61d4bcdc700ada3a2cac1749276878e0ba5c8b10
taehoy <rethnoly@gmail.com>
2025-01-03 18:55:33 +0900

    feat: OwnedItemRepository 추상화 및 구현체 분리

--- src/main/java/com/example/jungleroyal/repository/OwnedItemRepository.java
+++ src/main/java/com/example/jungleroyal/repository/OwnedItemRepository.java
@@ -1,14 +1,10 @@
 package com.example.jungleroyal.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-
+import java.util.Arrays;
 import java.util.List;
 
-@Repository
-public interface OwnedItemRepository extends JpaRepository<OwnedItemJpaEntity, Long> {
-    @Query("SELECT o FROM OwnedItemJpaEntity o WHERE o.inventory.user.id = :userId")
-    List<OwnedItemJpaEntity> findOwnedItemsByUserId(@Param("userId") Long userId);
+public interface OwnedItemRepository {
+    List<OwnedItemJpaEntity> findOwnedItemsByUserId(Long id);
+    void save(OwnedItemJpaEntity ownedItem);
+
 }
