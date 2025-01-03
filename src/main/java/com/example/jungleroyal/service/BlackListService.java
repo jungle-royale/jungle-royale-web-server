b7acd35cd040359eebfe2d51c7884cf0c95c1c4e
taehoy <rethnoly@gmail.com>
2025-01-03 17:24:06 +0900

    feat: BlackListService - TokensGenerator 의존성 주입 제거
    사용하지 않음

--- src/main/java/com/example/jungleroyal/service/BlackListService.java
+++ src/main/java/com/example/jungleroyal/service/BlackListService.java
@@ -1,14 +1,12 @@
 package com.example.jungleroyal.service;
 
 import com.example.jungleroyal.repository.BlackListRepository;
-import com.example.jungleroyal.common.util.AuthTokensGenerator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class BlackListService {
-    private final AuthTokensGenerator authTokensGenerator;
     private final BlackListRepository blackListRepository;
 
     public boolean isBlackListToken(String refreshToken){
