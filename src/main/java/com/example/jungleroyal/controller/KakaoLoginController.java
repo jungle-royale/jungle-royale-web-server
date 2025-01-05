ec4d0304bb58072d5d364ae554bab92e6f6ed641
taehoy <rethnoly@gmail.com>
2025-01-06 00:09:49 +0900

    feat: 카카오 로그인 컨트롤러 코드 일부 제거
    카카오 토큰 발급 메서드 제거

--- src/main/java/com/example/jungleroyal/controller/KakaoLoginController.java
+++ src/main/java/com/example/jungleroyal/controller/KakaoLoginController.java
@@ -1,15 +1,16 @@
 package com.example.jungleroyal.controller;
 
-import com.example.jungleroyal.domain.OAuthKakaoToken;
-import com.example.jungleroyal.domain.auth.KakaoLoginResponse;
-import com.example.jungleroyal.service.BlackListService;
-import com.example.jungleroyal.service.KakaoAuthService;
 import com.example.jungleroyal.common.util.JwtTokenProvider;
+import com.example.jungleroyal.domain.auth.KakaoLoginResponse;
+import com.example.jungleroyal.service.KakaoAuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,7 +23,6 @@ public class KakaoLoginController {
 
     private final KakaoAuthService kakaoAuthService;
     private final JwtTokenProvider jwtTokenProvider;
-    private final BlackListService blackListService;
 
     /**
      * 카카오 로그인 API
@@ -32,7 +32,7 @@ public class KakaoLoginController {
      */
     @PostMapping("/api/auth/kakao/login")
     @Operation(summary = "kakao Login", description = "카카오 로그인, 회원가입, jwt발행")
-    public ResponseEntity<KakaoLoginResponse> kakaoCallback(@RequestBody Map<String, String> payload) {
+    public ResponseEntity<KakaoLoginResponse> kakaoLogin(@RequestBody Map<String, String> payload) {
         String code = payload.get("code"); // 클라이언트에서 전송한 인가코드
         // TODO: 에러메시지 클라이언트에게 전송하기
         KakaoLoginResponse kakaoLoginResponse = kakaoAuthService.loginWithKakao(code);
@@ -50,36 +50,4 @@ public class KakaoLoginController {
 
         return ResponseEntity.ok(responseBody);
     }
-
-    /**
-     * RefreshToken 발급
-     * @param refreshToken
-     * @return access_token , refresh_token, expires_in
-     */
-//    @PostMapping("/api/auth/kakao/refresh-token")
-//    public ResponseEntity<Map<String, String>> refreshAccessToken(@RequestHeader("authorization_refresh") String refreshToken) {
-//        if (refreshToken == null || refreshToken.isEmpty()) {
-//            return ResponseEntity.badRequest().body(Map.of("error", "Refresh token is missing"));
-//        }
-//
-//        // 블랙리스트에 있는 토큰인지 확인하고 예외 발생
-//        if (blackListService.isBlackListToken(refreshToken)) {
-//            throw new IllegalArgumentException("[ERROR] 해당 Refresh Token은 블랙리스트에 등록되어 사용이 불가능합니다.");
-//        }
-//
-//        String realRefreshToken = refreshToken.substring(7);
-//        // 리프레시 토큰 발급
-//        OAuthKakaoToken oAuthKakaoToken = kakaoAuthService.getKakaoRefreshToken(realRefreshToken);
-//
-//        // Todo : 리프레시 토큰 재발급을 위한 객체 생성 후 이용하기
-//        Map<String, String> responseBody = new HashMap<>();
-//
-//        responseBody.put("expires_in", String.valueOf(oAuthKakaoToken.getExpires_in()));
-//        responseBody.put("refresh_token", oAuthKakaoToken.getRefresh_token());
-//        responseBody.put("access_token", oAuthKakaoToken.getAccess_token());
-//        responseBody.put("is_guest", "false");
-//        responseBody.put("success", "true");
-//
-//        return ResponseEntity.ok(responseBody);
-//    }
 }
