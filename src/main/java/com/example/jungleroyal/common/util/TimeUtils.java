69c582f2d32c67931fb5992dc4984508049dfe5f
taehoy <rethnoly@gmail.com>
2025-01-05 23:48:17 +0900

    feat: UTC 시간 추출 메서드 생성

--- src/main/java/com/example/jungleroyal/common/util/TimeUtils.java
+++ src/main/java/com/example/jungleroyal/common/util/TimeUtils.java
@@ -10,4 +10,16 @@ public class TimeUtils {
                 .atZoneSameInstant(ZoneId.of("Asia/Seoul")) // KST로 변환
                 .toLocalDateTime(); // LocalDateTime 반환
     }
+
+    /**
+     * KST(Asia/Seoul)를 UTC로 변환
+     *
+     * @return 변환된 UTC LocalDateTime
+     */
+    public static LocalDateTime createUtc() {
+        LocalDateTime kstTime = LocalDateTime.now();
+        return kstTime.atZone(ZoneId.of("Asia/Seoul")) // KST로 해석
+                .withZoneSameInstant(ZoneOffset.UTC) // UTC로 변환
+                .toLocalDateTime(); // LocalDateTime 반환
+    }
 }
