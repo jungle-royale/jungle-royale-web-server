b3fe2c598912357a186e8d05692e51815aa1a6ef
taehoy <rethnoly@gmail.com>
2025-01-08 01:19:51 +0900

    feat: 룸 상태에 따라 리스트 출력 여부 수정
    수정 전 : WAITING을 제외한 모든 방 출력X
    수정 후 : END를 제외한 모든 방 출력 O

--- src/main/java/com/example/jungleroyal/common/types/RoomStatus.java
+++ src/main/java/com/example/jungleroyal/common/types/RoomStatus.java
@@ -9,7 +9,6 @@ public enum RoomStatus {
     public boolean canShow() {
         // 게임이 끝난건 표시할 필요가 없다.
         // 게임 중인건 reconnection이 가능하기 때문에 보여야 한다.
-        // RUNNING도 보여줘야 하는데, 아직 reconnection 구현이 안되어서 일단 노출 X
-        return this == WAITING;
+        return this != END;
     }
 }
