afb92700e533025cae89ae8a25381d2086d82f2a
taehoy <rethnoly@gmail.com>
2025-01-08 16:44:19 +0900

    feat: 필터 우회 URL 별도 관리 로직 추가
    - YAML/Properties 파일에서 우회 URL을 관리할 수 있는 구조 확장

--- src/main/java/com/example/jungleroyal/common/config/BypassUrlConfig.java
+++ src/main/java/com/example/jungleroyal/common/config/BypassUrlConfig.java
@@ -1,16 +1,23 @@
 package com.example.jungleroyal.common.config;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+
+@Getter
+@Setter
 @Component
+@ConfigurationProperties(prefix = "jwt.bypass")
 public class BypassUrlConfig {
+
+    private List<String> urls; // YAML의 jwt.bypass.urls 값을 매핑
+
     public List<String> getBypassUrls() {
-        return List.of(
-                "/api/game/**",
-                "/api/posts/list"
-        );
+        return urls;
     }
 
     public boolean isBypassUrl(String requestUri) {
