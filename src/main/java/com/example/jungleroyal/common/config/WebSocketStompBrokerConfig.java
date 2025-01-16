16a7379dc1f8c82990d3ae83e492708070e9914a
taehoy <rethnoly@gmail.com>
2025-01-16 19:33:01 +0900

    feat: stomp 웹소켓 AllowedOrigins 처리

--- src/main/java/com/example/jungleroyal/common/config/WebSocketStompBrokerConfig.java
+++ src/main/java/com/example/jungleroyal/common/config/WebSocketStompBrokerConfig.java
@@ -4,9 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -38,7 +42,7 @@ public class WebSocketStompBrokerConfig implements WebSocketMessageBrokerConfigu
                 // 클라이언트가 WebSocket에 연결하기 위한 엔드포인트를 "/ws-stomp"로 설정합니다.
                 .addEndpoint("/ws-stomp")
                 // 클라이언트의 origin을 명시적으로 지정
-                .setAllowedOrigins("*")
+                .setAllowedOriginPatterns("*") // Spring 5.3 이상에서 사용 가능
                 // WebSocket을 지원하지 않는 브라우저에서도 SockJS를 통해 WebSocket 기능을 사용할 수 있게 합니다.
                 .withSockJS();
     }
